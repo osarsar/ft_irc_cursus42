842df0107278240ff27a69790c800aefb9341e77
Othman Chekairi <otchekai@e3r1p3.1337.ma>
2024-01-17 05:03:22 +0100

    hh

--- ft_irc/src/server/servsocket.cpp
+++ ft_irc/src/server/servsocket.cpp
@@ -1,15 +1,48 @@
-#include "servsocket.hpp"
+#include "../../inc/servsocket.hpp"
+#include "../../inc/channel.hpp"
 
-const char* SERVSOCKET::ErrorOnSocket::what() const throw()
+//--------------------------HANDEL ERRORS--------------------------------//
+
+const char* SERVSOCKET::ErrorOnMySocket::what() const throw()
 {
-    return "Error";
+    return RED"Failed To Create"RESET;
 }
+const char* SERVSOCKET::ErrorOnMyBind::what() const throw()
+{
+    return RED"Failed To Bind"RESET;
+}
+
+const char* SERVSOCKET::ErrorOnMyListen::what() const throw()
+{
+    return RED"Failed To Listening"RESET;
+}
+
+const char* SERVSOCKET::ErrorOnMyRecv::what() const throw()
+{
+    return RED"Client Is Disconnected"RESET;
+}
+
+const char* SERVSOCKET::ErrorOnMyAccept::what() const throw()
+{
+    return RED"Failed To Accept"RESET;
+}
+
+const char* SERVSOCKET::ErrorOnMySend::what() const throw()
+{
+    return RED"Failed To Send"RESET;
+}
+const char* SERVSOCKET::ErrorOnPassword::what() const throw()
+{
+    return "";
+}
+
+//----------------------------------------------------------------------//
 
 int SERVSOCKET::mysocket(int ipvs, const int type)
 {
     socket_server = socket(ipvs, type, 0);
     if (socket_server == -1)
-        throw ErrorOnSocket();
+        throw ErrorOnMySocket();
     return socket_server;
 }
 
@@ -19,10 +52,12 @@ void SERVSOCKET::mybind(std::string ip, int port)
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
 
+    int op = 1;
+    setsockopt(socket_server,SOL_SOCKET,SO_REUSEADDR,(void *)&op, sizeof(op));
     if (bind(socket_server, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
     {
         close(socket_server);
-        throw ErrorOnSocket();
+        throw ErrorOnMyBind();
     }
 }
 
@@ -31,7 +66,7 @@ void SERVSOCKET::mylisten(int nb_client)
     if (listen(socket_server, nb_client) == -1)
     {
         close(socket_server);
-        throw ErrorOnSocket();
+        throw ErrorOnMyListen();
     }
 }
 
@@ -46,7 +81,7 @@ int SERVSOCKET::myaccept()
     if (socket_client == -1)
     {
         close(socket_server);
-        throw ErrorOnSocket();
+        throw ErrorOnMyAccept();
     }
     mysend(socket_client, "USERNAME: ");
     username = myrecv(1024, socket_client);
@@ -55,7 +90,14 @@ int SERVSOCKET::myaccept()
     mysend(socket_client, "PASSWORD: ");
     password = myrecv(1024, socket_client);
     database[username] = password;
-    mysend(socket_client, "------- WELCOME TO THE SERVER ------");
+    // if (password != servpass)
+    // {
+    //     mysend(socket_client, RED"INCORRECT PASSWORD"RESET);
+    //     close(socket_client);
+    //     throw ErrorOnPassword();
+    // }
+
+    mysend(socket_client, GREEN"------- WELCOME TO THE SERVER ------\n"RESET);
     return socket_client;
 }
 
@@ -67,9 +109,7 @@ std::string SERVSOCKET::myrecv(unsigned int size, int fd)
     num_read = recv(fd, buffer, sizeof(buffer), 0);
     buffer[num_read] = '\0';
     if (num_read <= 0)
-    {
-        throw ErrorOnSocket();
-    }
+        throw ErrorOnMyRecv();
     std::string str(buffer);
     return str;
 }
@@ -77,7 +117,7 @@ std::string SERVSOCKET::myrecv(unsigned int size, int fd)
 void SERVSOCKET::mysend(int fd, std::string data)
 {
     if (send(fd, data.c_str(), data.length(), 0) == -1)
-        throw ErrorOnSocket();
+        throw ErrorOnMySend();
 }
 
 void SERVSOCKET::show()
@@ -100,4 +140,18 @@ void POLLFD::push(int fd, short events, short revents)
     sock_fd.events = events;
     sock_fd.revents = revents;
     vector.push_back(sock_fd);
+}
+
+channel *SERVSOCKET::add_channel(std::string name, channel *Channel) {
+    Channel = new channel(name);
+        channel_vec.push_back(Channel);
+    return Channel;
+}
+
+std::vector<int> POLLFD::getFds() const {
+    std::vector<int> fds;
+    for (std::vector<struct pollfd>::const_iterator it = this->vector.begin(); it != vector.end(); ++it) {
+        fds.push_back(it->fd);
+    }
+    return fds;
 }
